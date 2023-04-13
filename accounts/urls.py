b328cecb9ef5f3597b2d35703502e32c8ef2cb45
Kurman11 <skedyd45@naver.com>
2023-04-13 15:34:56 +0900

    Accounts/ views

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 app_name='accounts'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('signup/',views.signup, name='signup'),
+    path('update/', views.update, name='update'),
+    path('delete/', views.delete, name='delete'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('<int:account_pk>/detail/', views.detail, name='detail'),
+    path('password/',views.password_change,name='password_change'),
+    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,89 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from .forms import CustomUserChangeForm,CustomUserCreationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth import update_session_auth_hash
 # Create your views here.
-def index(request):
-    return render(request, 'accounts/index.html')
+
+
+def signup(request):
+    if request.method =='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request,user)
+            return redirect('reviews:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request,'accounts/signup.html',context)
+
+
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance = request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('reviews:index')
+    else:
+        form = CustomUserChangeForm(instance = request.user)
+    context = {
+        'form' : form,
+    }
+
+    return render(request,'accounts/update.html',context)
+
+
+def delete(request):
+    request.user.delete()
+    auth_logout(request)
+    return redirect('reviews:index')
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('reviews:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'accounts/login.html',context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('reviews:index')
+
+
+def detail(request,account_pk):
+    account = get_user_model().objects.get(pk=account_pk)
+    reviews = account.review_set.all()
+    context = {
+        'account': account,
+        'reviews': reviews,
+    }
+    return render(request,'accounts/detail.html',context)
+
+
+def password_change(request):
+    if request.method =='POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request,user)
+            return redirect('reviews:index')
+
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form':form,
+    }
+    return render(request,'accounts/password_change.html',context)
