3206ff1cea74182a81056dff191418b560517e71
rloldl <you@example.comii319@naver.comgit config --global user.email you@example.comii319@naver.com>
2023-04-13 17:45:59 +0900

    Create templates

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,79 @@
 from django import forms
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        label='아이디',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '아이디',
+            },
+        ),
+    )
+    
+    email = forms.EmailField(
+        label='이메일',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '이메일@example.com',
+            },
+        ),
+    )
+    
+    first_name = forms.CharField(
+        label='이름',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '이름',
+            },
+        ),
+    )
+
+    last_name = forms.CharField(
+        label='성',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '성',
+            },
+        ),
+    )
+
+
     birthday = forms.DateField(
-        widget = forms.DateInput(
+        label='생년월일',
+        widget=forms.DateInput(
             attrs={
                 'type': 'date',
-            }
-        )
+                'class': 'form-control', 
+            },
+        ),
+    )
+
+    password1 = forms.CharField(
+        label='비밀번호',
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '비밀번호',
+            },
+        ),
+        help_text='8자 이상 영문 대 소문자 숫자, 특수문자를 사용하세요.',
+    )
+
+    password2 = forms.CharField(
+        label='비밀번호 확인',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '비밀번호 확인',
+            },
+        ),
     )
 
     class Meta(UserCreationForm.Meta):
@@ -17,15 +82,66 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class CustomUserChangeForm(UserChangeForm):
-    birthday = forms.DateField(
-        widget = forms.DateInput(
+    email = forms.EmailField(
+        label='이메일',
+        widget=forms.TextInput(
             attrs={
-                'type': 'date',
-            }
-        )
+                'class': 'form-control',
+            },
+        ),
+    )
+    
+    first_name = forms.CharField(
+        label='이름',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+            },
+        ),
     )
 
-    password= None
+    last_name = forms.CharField(
+        label='성',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+            },
+        ),
+    )
+    
+    birthday = forms.DateField(
+        label='생년월일',
+        widget=forms.DateInput(
+            attrs={
+                'class': 'form-control',
+                'type': 'date',
+            },
+        ),
+    )
+
+    password = None
+
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
-        fields = ('email', 'username', 'birthday',)
+        fields = ('email', 'first_name', 'last_name', 'birthday',)
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        label='아이디',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+            },
+        ),
+    )
+
+    password = forms.CharField(
+        label='비밀번호',
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+            },
+        ),
+    )
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Review, Comment
+from .forms import ReviewForm, CommentForm
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'accounts/index.html')
+    reviews = Review.objects.all()
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'reviews/index.html', context)
+
+
+def detail(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    form = CommentForm()
+    comments = review.comment_set.all()
+    context = {
+        'review': review,
+        'form': form,
+        'comments': comments,
+    }
+    return render(request, 'reviews/detail.html', context)
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('reviews:detail', review.pk)
+    else:
+        form = ReviewForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'reviews/create.html', context)
+
+
+@login_required
+def update(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.user == review.user:
+        if request.method == 'POST':
+            form = ReviewForm(request.POST, instance=review)
+            if form.is_valid():
+                form.save()
+                return redirect('reviews:detail', review.pk)
+        else:
+            form = ReviewForm(instance=review)
+        context = {
+            'form': form,
+            'review': review,
+        }
+        return render(request, 'reviews/update.html', context)
+    else:
+        return redirect('reviews:detail', review.pk)
+    
+
+@login_required
+def delete(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.user == review.user:
+        review.delete()
+    return redirect('reviews:index')
+
+
+# @login_required
+# def create_comment(request, review_pk):
+    
